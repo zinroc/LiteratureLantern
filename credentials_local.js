0c6b75b5441d30824b6c51fae0b9e1fe798061e5
zinroc <admin@Admins-MacBook-Air-3.local>
2017-01-27 23:01:33 -0500

    heroku db credentials

--- credentials_local.js
+++ credentials_local.js
@@ -1,17 +1,16 @@
+
 module.exports = {
 
 //localhost
 	//PG_CON_STRING: "://postgres:@localhost:8000/garbage"
-	/**
 	PG_CON_STRING: {
 		host: 'localhost',
 		user: 'postgres', 
 		password: '',
 		database: 'literaturelantern'
 	}
-	**/
 	
-
-	PG_CON_STRING: "postgres://gvchctjoohpbbn:4ffd30ed75a89119b16df295ff9e29c207741a960099b4ceb1d0691bedaaed05@ec2-54-243-187-133.compute-1.amazonaws.com:5432/d3r8q118aiunso"
+//emergence
+//	PG_CON_STRING: "postgres://cosyudldjdolpk:Z0qqJa7gymZliw2sf3Jdpp4Z34@ec2-54-235-95-188.compute-1.amazonaws.com:5432/d9fgvijtf72tl2"
 
 };
